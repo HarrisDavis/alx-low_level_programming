f11acc76bb4335b51b5301a09e7ece3034f22e9d
HarrrisDavis <okattaharris@gmail.com>
2022-10-17 18:15:16 -0700

    good

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,7 +9,7 @@ int main(void)
 	int num;
 	char letter;
 
-	for (num = 0; int < 10; num++)
+	for (num = 0; in < 10; num++)
 		putchar ((num 10) + '0');
 
 	for (letter = 'a'; letter <= 'f'; letter++)
